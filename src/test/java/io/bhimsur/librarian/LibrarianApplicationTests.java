20eeaee79f91ae87d4f7468aa30f822ef4c758b8
bhimsur <bhimsur>
2023-06-04 21:46:47 +0700

    init

---
+++ src/test/java/io/bhimsur/librarian/LibrarianApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bhimsur.librarian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
