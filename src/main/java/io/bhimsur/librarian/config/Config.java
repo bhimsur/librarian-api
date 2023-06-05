acfca3ff528ec246b3e1b02aaf6c7d9881593086
bhimsur <bhimsur>
2023-06-05 16:25:57 +0700

    add config

---
+++ src/main/java/io/bhimsur/librarian/config/Config.java
@@ -0,0 +1,20 @@
+package io.bhimsur.librarian.config;
+
+import io.bhimsur.librarian.filter.AuthenticationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+    @Bean
+    public AuthenticationInterceptor authenticationInterceptor() {
+        return new AuthenticationInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor());
+    }
+}
